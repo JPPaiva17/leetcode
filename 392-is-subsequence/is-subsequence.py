0203c419d758330b748c2a0b5cd55aa1252d6073
João Pedro Paiva <168781926+JPPaiva17@users.noreply.github.com>
2026-09-15 13:00:42 -0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.54%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,7 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        countEqual = 0
-        for c in t:
-            if countEqual < len(s) and s[countEqual] == c:
-                countEqual += 1
-        return countEqual == len(s)
+        pointerS = 0
+        pointerT = 0
+        while pointerT < len(t) and pointerS < len(s):
+            if s[pointerS] == t[pointerT]:
+                pointerS += 1
+            pointerT += 1
+
+        return pointerS == len(s)
