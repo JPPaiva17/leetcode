46ef0ccbc56ee87cee276d8e8377c2202c64041b
João Pedro Paiva <168781926+JPPaiva17@users.noreply.github.com>
2026-09-16 14:15:39 -0300

    Time: 18 ms (18.75%) | Memory: 19.2 MB (94.70%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hashMapS = {}
+        hashMapT = {}
+        for i in range(len(s)):
+            if s[i] not in hashMapS:
+                hashMapS[s[i]] = 1
+            else:
+                hashMapS[s[i]] += 1
+            if t[i] not in hashMapT:
+                hashMapT[t[i]] = 1
+            else:
+                hashMapT[t[i]] += 1
+        return hashMapS == hashMapT
+        
