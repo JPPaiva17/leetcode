b39b81e1920fdea8a7e5ca22567c7c81afc729f7
João Pedro Paiva <168781926+JPPaiva17@users.noreply.github.com>
2026-09-21 19:28:08 -0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (93.00%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -7,22 +7,20 @@ from collections import deque
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: TreeNode | None) -> int:
-        
-        if root.left == None and root.right == None:
+        if not root:
             return 0
     
         totalSum = 0
         queue = deque([root])
+
         while queue:
             node = queue.popleft()
-            left_son = node.left
+            if not node:
+                continue
+
+            if node.left and not node.left.left and not node.left.right:
+                totalSum += node.left.val
             
-            if left_son and left_son.right == None and left_son.left == None:
-                totalSum += left_son.val
-            
-            if node.left:
-                queue.append(node.left)
-            
-            if node.right:
-                queue.append(node.right)
+            queue.append(node.left)
+            queue.append(node.right)
         return totalSum           
